8f91f2c0cea488cd03b458f10925c2421328a795
David Berry <dberry83702@yahoo.com>
2025-05-11 21:51:48 -0500

    Added express-limit library as rate limiter and implemented in routers

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const limit = require("express-limit").limit;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get(
+  '/', 
+  limit({
+    max: 5, // 5 requests
+    period: 60 * 1000, // per minute (60 seconds)
+  }),
+  function(req, res, next) {
   res.render('index', { title: 'Home - Financial Calculator' });
 });
 
--- routes/loan-calculator.js
+++ routes/loan-calculator.js
@@ -1,85 +1,93 @@
 var express = require('express');
 var router = express.Router();
+const limit = require("express-limit").limit;
 
 const { body,validationResult } = require("express-validator");
 const numeral = require('numeral');
 
 
 /* GET loan calculator page. */
-router.get('/', function(req, res, next) {
+router.get(
+    '/', 
+    limit({
+        max: 5, // 5 requests
+        period: 60 * 1000, // per minute (60 seconds)
+      }),    
+    function(req, res, next) {
+        var formData = {
+            principal: 10000,        
+            interestRate: 5.75,
+            years: 5
+        }
 
-    var formData = {
-        principal: 10000,        
-        interestRate: 5.75,
-        years: 5
-    }
-
-    res.render('loan-calculator', { title: 'Loan Calculator', formData: formData });
+        res.render('loan-calculator', { title: 'Loan Calculator', formData: formData });
   });
 
 
 
 
-  router.post('/', [
-
-    // Validate and sanitize fields.
+  router.post(
+    '/', 
+    limit({
+        max: 5, // 5 requests
+        period: 60 * 1000, // per minute (60 seconds)
+      }),     
+    [
+        // Validate and sanitize fields.
+        body('principal', 'Principal amount not be empty.').trim().isLength({ min: 1 }),
+        body('principal', 'Principal must be between $1 and $1,000,000.').isFloat({ min: 1.0, max: 1000000}),
+        body('interestRate', 'Interest rate must not be empty.').trim().isLength({ min: 1 }),
+        body('interestRate', 'Interest rate must be between 0.1 and 20%.').isFloat({ min: 0.1, max: 20}),
+        body('years', 'Years must not be empty.').trim().isLength({ min: 1 }),
+        body('years', 'Years must be between 1 and 40.').isInt({ min: 1, max: 40}),
     
-    body('principal', 'Principal amount not be empty.').trim().isLength({ min: 1 }),
-    body('principal', 'Principal must be between $1 and $1,000,000.').isFloat({ min: 1.0, max: 1000000}),
+        (req, res, next) => {
+            const errors = validationResult(req);
 
-    body('interestRate', 'Interest rate must not be empty.').trim().isLength({ min: 1 }),
-    body('interestRate', 'Interest rate must be between 0.1 and 20%.').isFloat({ min: 0.1, max: 20}),
+            // local variables to use in this method
+            var principal = parseFloat(req.body.principal);
+            var interestRate = parseFloat(req.body.interestRate) / 100;  // Interest comes in as Percent - we need decimal value
+            var years = parseInt(req.body.years);
 
-    body('years', 'Years must not be empty.').trim().isLength({ min: 1 }),
-    body('years', 'Years must be between 1 and 40.').isInt({ min: 1, max: 40}),
-    
-    (req, res, next) => {
-        const errors = validationResult(req);
+            // This is the data that was passed in
+            var formData = {
+                principal: req.body.principal,
+                interestRate: req.body.interestRate,
+                years: req.body.years
+            }
 
-        // local variables to use in this method
-        var principal = parseFloat(req.body.principal);
-        var interestRate = parseFloat(req.body.interestRate) / 100;  // Interest comes in as Percent - we need decimal value
-        var years = parseInt(req.body.years);
+            if(!errors.isEmpty()) {
+                // There were errors, so retrun back the form data and errors so the user can fix things
+                res.render('loan-calculator', { title: 'Loan Calculator', formData: formData, errors: errors.array() });
+            }
 
-        // This is the data that was passed in
-        var formData = {
-            principal: req.body.principal,
-            interestRate: req.body.interestRate,
-            years: req.body.years
-        }
+            var paymentsPerYear = 12;
 
-        if(!errors.isEmpty()) {
-            // There were errors, so retrun back the form data and errors so the user can fix things
-            res.render('loan-calculator', { title: 'Loan Calculator', formData: formData, errors: errors.array() });
-        }
-
-        var paymentsPerYear = 12;
-
-        var numerator = (interestRate * principal);
-        var denominator = paymentsPerYear * (1 - ((1 + interestRate / paymentsPerYear) ** -(paymentsPerYear * years)))
+            var numerator = (interestRate * principal);
+            var denominator = paymentsPerYear * (1 - ((1 + interestRate / paymentsPerYear) ** -(paymentsPerYear * years)))
         
-        var monthlyPayment = numerator / denominator;
-        var totalPayments = monthlyPayment * years * paymentsPerYear;
-        var totalInterest = totalPayments - principal;
+            var monthlyPayment = numerator / denominator;
+            var totalPayments = monthlyPayment * years * paymentsPerYear;
+            var totalInterest = totalPayments - principal;
 
-        var summaryData = {
-            principal: numeral(formData.principal).format('$0,0.00'),
-            interestRate: formData.interestRate,
-            years: formData.years,
+            var summaryData = {
+                principal: numeral(formData.principal).format('$0,0.00'),
+                interestRate: formData.interestRate,
+                years: formData.years,
 
-            monthlyPayment: numeral(monthlyPayment).format('$0,0.00'),
-            totalPayments: numeral(totalPayments).format('$0,0.00'),
-            totalInterest: numeral(totalInterest).format('$0,0.00'),
-        }
+                monthlyPayment: numeral(monthlyPayment).format('$0,0.00'),
+                totalPayments: numeral(totalPayments).format('$0,0.00'),
+                totalInterest: numeral(totalInterest).format('$0,0.00'),
+            }
 
-        var results = {
-            summary: summaryData,
-        }
+            var results = {
+                summary: summaryData,
+            }
 
-        res.render('loan-calculator', { title: 'Loan Calculator', formData: formData, results: results, errors: [] });
-      }
-      
-]);
+            res.render('loan-calculator', { title: 'Loan Calculator', formData: formData, results: results, errors: [] });
+        }      
+    ]
+);
 
 
 
